85143fa77faa328657013d4ed4e80db63d1a340a
Roghashin Timbiti <rtimbiti@gmail.com>
2021-04-07 19:35:26 +0000

    Add Fonts loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,9 +27,13 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(png|jpe?g|gif|svg)$/i,
                 loader: 'file-loader',
-             }
+             },
+             {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ]
     },
     resolve: {
